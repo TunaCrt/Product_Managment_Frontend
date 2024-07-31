6ee77e8d842ae362142ae2fef5eaa5f0244bd72d
ibrahim Tuna <tuna.cort52@gmail.com>
2024-07-31 16:57:29 +0300

    update

--- product_ui/src/component/Navbar.jsx
+++ product_ui/src/component/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
+        
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-success">
                 <div className="container-fluid">
--- product_ui/src/service/product.service.js
+++ product_ui/src/service/product.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8082"; 
+const API_URL = "http://localhost:8080"; 
 
 class ProductService {
 
